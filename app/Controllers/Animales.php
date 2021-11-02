41983ece989f55c3caf728fd8b4d2a0e7370f51f
EstefaniaHo2122 <estefania.ho2122@gmail.com>
2021-11-02 22:31:22 +0100

    Avance animales-Productos
    Se carga proyecto, con avance en agregar, eliminar y buscar tanto animales como productos.

---
+++ app/Controllers/Animales.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controllers;
+//Se importa el modelo de datos
+use App\Models\AnimalModelo;
+
+class Animales extends BaseController
+{
+    public function index(){
+        return view('registroAnimal');
+    }
+    public function registrar (){
+        // recibo los datos enviados desde el formulario(desde la vista)
+        //Lo que tengo dentro de getPost("") es el name que puse en cada input
+        $nombre=$this->request->getPost("nombre");
+        $edad=$this->request->getPost("edad");       
+        $descripcion=$this->request->getPost("descripcion");
+        $tipo=$this->request->getPost("tipo");
+        print_r($nombre);
+        if($this->validate('animal')){
+        //Crear un arreglo asociativo con los datos del punto 1
+        $datos=array(
+            "nombre"=>$nombre,
+            "edad"=>$edad,            
+            "descripcion"=>$descripcion,
+            "tipo"=>$tipo
+        );
+        //4. intentamos grabar los datos en BD
+        try{
+           
+            $modelo=new AnimalModelo();
+            $modelo->insert($datos);
+            return redirect()->to(site_url('/animales/registro'))->with('mensaje',"El registro fue exitoso");
+
+
+        }catch(\Exception $error){
+            return redirect()->to(site_url('/animales/registro'))->with('mensaje',$error->getMessage());
+        }
+
+        }else{
+        $mensaje="tienes datos pendientes";
+        return redirect()->to(site_url('/animales/registro'))->with('mensaje',$mensaje);
+        }        
+    }
+
+    public function buscar(){
+        try{
+            $modelo=new AnimalModelo();
+            $resultado=$modelo->findAll();
+            $animales=array('animales'=>$resultado);
+            return view('listaAnimales',$animales);
+        }catch(\Exception $error){
+            return redirect()->to(site_url('/animales/registro'))->with('mensaje',"sara".$error->getMessage());
+        }
+   }
+
+    public function eliminar($id){
+
+       try{
+        $modelo=new AnimalModelo();
+        $modelo->where('id',$id)->delete();
+        return redirect()->to(site_url('/animales/registro'))->with('mensaje',"Eliminaciòn exitosa");
+        }catch(\Exception $error){
+            return redirect()->to(site_url('/animales/registro'))->with('mensaje',$error->getMessage());
+
+        }
+    }
+}
---
+++ app/Controllers/Productos.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controllers;
+//Se importa el modelo de datos
+use  App\Models\ProductoModelo;
+
+class Productos extends BaseController{
+    
+    public function index(){
+        return view('registroProducto');
+    }
+
+    public function registrar(){
+       
+        //1. Recibo todos los datos enviados desde el formulario (vista)
+        //Lo que tengo entre getPost("") es el name que puse a cada input
+        $producto=$this->request->getPost("producto");
+        $foto=$this->request->getPost("foto");
+        $precio=$this->request->getPost("precio");
+        $descripcion=$this->request->getPost("descripcion");
+        $tipo=$this->request->getPost("tipo");
+
+        //2. Valido que llego
+        if($this->validate('producto')){
+            $datos=array(
+                //3. Se organizan los datos en un arreglo Los nombres deben coincidir con los de la base de datos.
+                "producto"=>$producto,
+                "foto"=>$foto,
+                "precio"=>$precio,
+                "descripcion"=>$descripcion,
+                "tipo"=>$tipo                
+            );
+
+            //4. intentamos grabar los datos en BD 
+            try{
+                $modelo=new ProductoModelo();
+                $modelo->insert($datos);
+                return redirect()->to(site_url('/productos/registro'))->with('mensaje',"El registro fue exitoso");
+
+            }catch(\Exception $error){
+                return redirect()->to(site_url('/productos/registro'))->with('mensaje',$error->getMessage());
+            }
+
+        }else{
+            $mensaje="tienes datos pendientes";
+            return redirect()->to(site_url('/productos/registro'))->with('mensaje',$mensaje);
+        }        
+        
+    }
+
+    public function buscar(){
+        try{
+            $modelo=new ProductoModelo();
+            $resultado=$modelo->findAll();
+            $productos=array('productos'=>$resultado);
+            return view('listaProductos',$productos);
+
+
+        }catch(\Exception $error){
+            return redirect()->to(site_url('/productos/registro'))->with('mensaje',$error->getMessage());
+
+        }
+       
+    }
+
+    public function eliminar($id){
+
+       try{
+
+        $modelo=new ProductoModelo();
+        $modelo->where('id',$id)->delete();
+        return redirect()->to(site_url('/productos/registro'))->with('mensaje',"exito eliminando el producto");
+    }catch(\Exception $error){
+        return redirect()->to(site_url('/productos/registro'))->with('mensaje',$error->getMessage());
+
+    }
+}
+    }
